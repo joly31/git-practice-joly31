9cf715b0c355c07987f77446dde77c104f363f29
joly31 <jolypaul2004@gmail.com>
2026-08-01 23:05:04 +0600

    Add basic error handling to calculator

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide 
 
 
 
@@ -10,3 +10,4 @@ print("Today's Date:", date.today())
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
+print(divide(20, 0))
--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,10 @@ def subtract(a, b):
 
 def multiply(a,b):
     return a * b
+
+
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
